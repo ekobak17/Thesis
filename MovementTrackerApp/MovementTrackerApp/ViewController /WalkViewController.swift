0f3d44d18fb30d5b718882f3d776713e44b10476
Ellie Kobak <63224714+ekobak17@users.noreply.github.com>
2021-10-12 13:32:48 -0700

    Connected buttons

---
+++ MovementTrackerApp/MovementTrackerApp/ViewController /WalkViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WalkViewController.swift
+//  MovementTrackerApp
+//
+//  Created by Ellie Kobak on 10/12/21.
+//
+
+import UIKit
+
+class WalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
